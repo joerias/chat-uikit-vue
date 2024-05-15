f782f237005737e246f47486a7eb29953dc062ec
LiJun <asterold@outlook.com>
2024-05-15 23:17:21 +0800

    appid

--- Vue3/Demo/src/main.ts
+++ Vue3/Demo/src/main.ts
@@ -1,14 +1,10 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import { TUIComponents, TUIChatKit } from './TUIKit';
-import {
-  TUIStore,
-  StoreName,
-  TUITranslateService,
-} from '@tencentcloud/chat-uikit-engine';
-import TUINotification from './TUIKit/components/TUINotification/index';
-import { locales } from './locales';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { TUIComponents, TUIChatKit } from "./TUIKit";
+import { TUIStore, StoreName, TUITranslateService } from "@tencentcloud/chat-uikit-engine";
+import TUINotification from "./TUIKit/components/TUINotification/index";
+import { locales } from "./locales";
 // ------ 以下是 TUIRoom 相关依赖的引入，如果您不需要使用 TUIRoom，请删除以下代码 ------
 // import { createPinia } from 'pinia';
 // import RoomVueI18n from './TUIKit/TUIRoom/locales/index';
@@ -20,10 +16,10 @@ app.use(router);
 // app.use(createPinia());
 // app.use(RoomVueI18n);
 // ----------------------------------------------------------------------------
-app.mount('#app');
+app.mount("#app");
 
-const SDKAppID = 0; // Your SDKAppID
-const secretKey = ''; // Your secretKey
+const SDKAppID = 1600036474; // Your SDKAppID
+const secretKey = "a2725bb024b32ca85f2b151072f779010aabe0b00fd7e84e273c89eb44aa5404"; // Your secretKey
 
 TUIChatKit.components(TUIComponents, app);
 TUIChatKit.init();
@@ -35,10 +31,10 @@ TUITranslateService.useI18n();
  * Init TUINotification configuration.
  */
 TUINotification.setNotificationConfiguration({
-  showPreviews: true,
-  allowNotifications: true,
-  notificationTitle: 'Tencent Cloud Chat',
-  notificationIcon: 'https://web.sdk.qcloud.com/im/demo/latest/faviconnew.png',
+	showPreviews: true,
+	allowNotifications: true,
+	notificationTitle: "Tencent Cloud Chat",
+	notificationIcon: "https://web.sdk.qcloud.com/im/demo/latest/faviconnew.png",
 });
 
 /**
@@ -46,11 +42,11 @@ TUINotification.setNotificationConfiguration({
  * This capability is only available in the web environment.
  */
 TUIStore.watch(StoreName.CHAT, {
-  newMessageList: (newMessageList: unknown) => {
-    if (Array.isArray(newMessageList)) {
-      newMessageList.forEach(message => TUINotification.notify(message));
-    }
-  },
+	newMessageList: (newMessageList: unknown) => {
+		if (Array.isArray(newMessageList)) {
+			newMessageList.forEach((message) => TUINotification.notify(message));
+		}
+	},
 });
 
 export { SDKAppID, secretKey };
